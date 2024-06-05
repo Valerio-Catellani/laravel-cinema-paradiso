ac29e83b259300471709eab941271838d82dd884
Valerio-Catellani <valeriocatellani@gmail.com>
2024-06-05 18:48:18 +0200

    added one to many for review

---
+++ database/migrations/2024_06_05_163751_add_movie_id_to_reviews_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->unsignedBigInteger('movie_id')->nullable()->after('id');
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->dropForeign(['reviews_movie_id_foreign']);
+            $table->dropColumn('movie_id');
+        });
+    }
+};
